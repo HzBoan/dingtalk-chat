efd3d964fcf1e2bbdd0df67f6e03d59915e306f3
HzBoan <1028151949@qq.com>
2018-07-21 22:26:30 +0800

    Initial commit

---
+++ src/main/java/com/dingtalk/chatbot/demo/TestConfig.java
@@ -0,0 +1,9 @@
+package com.dingtalk.chatbot.demo;
+
+/**
+ * Created by dustin on 2017/3/19.
+ */
+public class TestConfig {
+    //TODO use YOUR chatbot webhook
+    public static final String CHATBOT_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=4628f35ad2ba1cebeec1b145b04495da73687a659fdcc252cdfc59acd6595916";
+}
